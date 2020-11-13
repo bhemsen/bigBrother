f9e6f0e62c6b0c53a3f6d0fc1a9d19055179f3f3
bhemsen <bhemsen@medienwerft.de>
2020-11-13 07:45:13 +0100

    refoctoring

---
+++ Database.py
@@ -0,0 +1,70 @@
+import mysql.connector
+import time
+from mysql.connector import Error
+
+class Database:
+    #this parameters needs to be strings
+    def __init__(self, host, user, passwd, database):
+        self.host = host
+        self.user = user
+        self.passwd = passwd
+        self.database = database
+        
+        self.db = mysql.connector.connect(
+            host= self.host,
+            user= self.user,
+            passwd= self.passwd,
+            database= self.database
+            )
+
+        self.cursor = self.db.cursor(prepared=True)
+
+    
+    def insertTemperetureAndHumidity(self, dht11Instance):
+        sql = "INSERT INTO temperature values(0, CURRENT_DATE(), NOW(), %s, %s)"
+
+        while True:
+            result = dht11Instance.read()
+            while not result.is_valid():  # read until valid values
+                result = dht11Instance.read()
+            
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+            
+            temperature = str(result.temperature)
+            humidity = str(result.humidity)
+            
+            try:
+                self.cursor.execute(sql, (temperature,humidity,), True)
+                self.db.commit()
+                print ("Data committed")
+            
+            except mysql.connector.Error as error:
+                print("parameterized query failed {}".format(error))
+                self.db.rollback()   
+                time.sleep(15)
+
+    #days needs to be a string   
+    def cleanUp(self, days):
+        sql = "DELETE FROM temperature WHERE DATEDIFF(day, NOW()) <= %s"
+        self.days = days
+
+        while True:
+            try:
+                self.cursor.execute(sql, days, True)
+                self.db.commit()
+                print ("Data deleted")
+                    
+            except mysql.connector.Error as error:
+                print("parameterized query failed {}".format(error))
+                self.db.rollback()    
+                time.sleep(2500000)
+
+        
+
+    def close(self):
+        self.cursor.close()
+        self.db.close()
+
+        
+
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import mysql.connector
-import time
-from mysql.connector import Error
+import asyncio
 import RPi.GPIO as GPIO
 import dht11
+from Database import Database
 
-# initialize GPIO
+# initialize GPIOS
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
@@ -12,42 +11,8 @@ GPIO.cleanup()
 # read data using pin 14
 instance = dht11.DHT11(pin = 4)
 
+# initialize database connection
+db = Database("localhost", "webadmin", "password", "sensoro")
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="webadmin",
-    passwd="password",
-    database="sensoro"
-)
-
-curser = db.cursor(prepared=True)
-sql_insert_query = "INSERT INTO temperature values(0, CURRENT_DATE(), NOW(), %s, %s)"
-
-while True:
-
-    result = instance.read()
-
-    while not result.is_valid():  # read until valid values
-        result = instance.read()
-
-    print("Temperature: %-3.1f C" % result.temperature)
-    print("Humidity: %-3.1f %%" % result.humidity)
-
-    temperature = str(result.temperature)
-    humidity = str(result.humidity)
-
-
-    try:
-        curser.execute(sql_insert_query, (temperature,humidity,), True)
-        db.commit()
-        print ("Data committed")
-        
-    except mysql.connector.Error as error:
-        print("parameterized query failed {}".format(error))
-        db.rollback()    
-    time.sleep(15)
-
-curser.close()
-db.close()
-
-
+asyncio.run(db.insertTemperetureAndHumidity(instance))
+asyncio.run(db.cleanUp("7"))

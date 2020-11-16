79ce8cd1c18722f150afd32fc166f0fb63f2d0b3
bhemsen <bhemsen@medienwerft.de>
2020-11-16 10:44:47 +0100

    try stuff

---
+++ SQLSCRIPT.sql
@@ -0,0 +1,42 @@
+CREATE USER 'webadmin'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'webadmin'@'%';
+
+CREATE DATABASE sensoro;
+USE sensoro;
+
+CREATE TABLE `temperature` (
+  `ID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `day` date NOT NULL,
+  `time` time NOT NULL,
+  `temperature` varchar(16),
+  `humidity` varchar(16)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `sensoro`.`securitylevel` ( 
+  `ID` INT NOT NULL ,
+  `name` VARCHAR(16) NOT NULL , 
+  PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+
+INSERT INTO `securitylevel` (`ID`, `name`) 
+VALUES ('1', 'Employee'), ('2', 'Boss');
+
+
+CREATE TABLE `sensoro`.`rfid` ( 
+  `ID` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(32) NOT NULL , 
+  `securityLevel` INT NOT NULL , 
+  `rfid` JSON NOT NULL , 
+  PRIMARY KEY (`ID`)) ENGINE = InnoDB;
+
+ALTER TABLE `rfid` ADD FOREIGN KEY (`securityLevel`) 
+REFERENCES `securitylevel`(`ID`) 
+ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `rfid` ADD UNIQUE( `rfid`);
+
+CREATE TABLE `sensoro`.`entrylog` ( 
+  `ID` INT NOT NULL AUTO_INCREMENT , 
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+  `name` VARCHAR(32) NOT NULL, 
+  `rfid` VARCHAR(128) NOT NULL , 
+  `access` VARCHAR(32) NOT NULL , 
+  PRIMARY KEY (`ID`)) ENGINE = InnoDB;

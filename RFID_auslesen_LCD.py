f4a5d33eed098f0286dba73b926aa293e639638f
bhemsen <bhemsen@medienwerft.de>
2020-11-16 11:50:08 +0100

    scyxc

--- RFID_auslesen_LCD.py
+++ RFID_auslesen_LCD.py
@@ -58,6 +58,7 @@ def unknown():
 def compareKeyWithDatabaseKeys(key):
     db = Database("localhost", "webadmin", "password", "sensoro")
     result = db.getAllowdRFIDS()
+    print(result)
     for i in range(len(result)-1):
         allowedKey = ast.literal_eval(result[i][0])
         securityLevel = result[i][2]

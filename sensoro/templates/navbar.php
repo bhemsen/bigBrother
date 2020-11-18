fc4c93eebd0a7c5e3193afac981cceb35eae7831
bhemsen <bhemsen@medienwerft.de>
2020-11-18 15:11:21 +0100

    fix links

--- sensoro/templates/navbar.php
+++ sensoro/templates/navbar.php
@@ -6,10 +6,10 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="http://localhost/sensoro/index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="http://localhost/index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="http://localhost/sensoro/templates/setConfig.php">Config</a>
+        <a class="nav-link" href="http://localhost/templates/setConfig.php">Config</a>
       </li>
     </ul>
   </div>

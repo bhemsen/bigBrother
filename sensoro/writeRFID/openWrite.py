20c1d22320827d422a171b061f8dd68010f53ad3
bhemsen <bhemsen@medienwerft.de>
2020-11-18 22:02:16 +0100

    imprting write

--- sensoro/writeRFID/openWrite.py
+++ sensoro/writeRFID/openWrite.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/home/pi/bigBrother/writeRFID')
 # enable debugging
 import cgitb
+
 cgitb.enable()
 
 print("Content-Type: text/plain;charset=utf-8")

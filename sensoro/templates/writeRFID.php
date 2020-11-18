3c79ddc684c6e7bc303d5040a1ce83833bfa4dae
bhemsen <bhemsen@medienwerft.de>
2020-11-18 21:38:30 +0100

    added input fiels

--- sensoro/templates/writeRFID.php
+++ sensoro/templates/writeRFID.php
@@ -2,12 +2,21 @@
 <p>
     Zum Überschreiben einer Karte den Button drücken und die Karte an den Sensor halten
 </p>
+<br>
 <small  class="form-text text-muted">
     Beachte: Das Programm das die Chips ausließt muss vorher beendet werden.
 </small>
 
 <form action="http://localhost/writeRFID/openWrite.py">
-    <button type="submit">Schreiben</button>
+    <div class="form-group">
+        <label for="name">Name: </label>
+        <input name="name" id="name" type="text">
+    </div>
+    <div class="form-group">
+        <label for="securityLevel">Sicherheitsstufe: </label>
+        <input name="securityLevel" id="securityLevel" type="number">
+    </div>
+    <button class="btn btn-primary" type="submit">Schreiben</button>
 </form>
 
 <?php require 'footer.php';?>

32b06d8b167164400550829dd4beebdaca74efd3
bhemsen <bhemsen@medienwerft.de>
2020-11-18 16:54:36 +0100

    fix loading order

--- sensoro/templates/temperature.php
+++ sensoro/templates/temperature.php
@@ -7,16 +7,14 @@
             <li>aktuelle Luftfeutchtigkeit: <span id="humidity"></span></li>
         </ul>
 
-    </section> 
+</section> 
 
 
-    <section>
-            <canvas id="chart"></canvas>
-            <canvas id="avarage-chart"></canvas>
-    </section>
+<section>
+        <canvas id="chart"></canvas>
+        <canvas id="avarage-chart"></canvas>
+</section>
   
-
-
-
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script type="application/javascript" src="temperature.js"></script>
 <?php require 'footer.php';?>
